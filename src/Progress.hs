5e4efd7c00f86aca0167c86f309cb8d78fd8076a
techno-tanoC <develop.tanoc@gmail.com>
2020-04-28 19:36:39 +0900

    update Progress

--- src/Progress.hs
+++ src/Progress.hs
@@ -3,9 +3,12 @@ module Progress where
 import Prelude hiding (Read, read)
 
 import Control.Concurrent.MVar
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 import Data.Word (Word64)
 
 import Write
+import Reporter
 
 newtype Progress a = Progress (MVar (ProgressInner a))
 
@@ -16,6 +19,12 @@ data ProgressInner a = ProgressInner {
     dest :: a
 } deriving (Show, Eq)
 
+instance Writable a => Writable (Progress a) where
+    write (Progress m) bs = modifyMVar_ m $ \inner -> do
+        let added = size inner + (fromIntegral . BS.length $ bs)
+        write (dest inner) bs
+        return $ inner { size = added }
+
 new :: String -> a -> IO (Progress a)
 new name a = fmap Progress . newMVar $ ProgressInner name 0 0 a
 
@@ -23,6 +32,7 @@ setContentLength :: (Progress a) -> Word64 -> IO ()
 setContentLength (Progress pg) cl = modifyMVar_ pg update
     where update inner = return $ inner { total = cl }
 
-progress :: (Progress a) -> Word64 -> IO ()
-progress (Progress pg) s = modifyMVar_ pg update
-    where update inner = return $ inner { size = size inner + s }
+toReporter :: (Progress a) -> IO Reporter
+toReporter (Progress m) = fmap convert $ readMVar m
+    where
+        convert (ProgressInner n t s _) = Reporter n t s
