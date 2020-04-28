dc6de788ce15cb843f001e106b8c1e9878037851
techno-tanoC <develop.tanoc@gmail.com>
2020-04-28 19:36:01 +0900

    update Write

--- src/Write.hs
+++ src/Write.hs
@@ -1,12 +1,19 @@
 module Write where
 
-import Data.ByteString
+import Control.Concurrent.MVar
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 import Data.Word
+import System.IO (Handle)
 
 class Writable a where
     write :: a -> ByteString -> IO ()
 
-data Cursor = Cursor {
-    pos :: Int,
-    content :: ByteString
-}
+instance Writable Handle where
+    write = BS.hPut
+
+newtype Buff = Buff (MVar ByteString)
+
+instance Writable Buff where
+    write (Buff m) bs = modifyMVar_ m $ \inner -> do
+        return $ BS.append inner bs
