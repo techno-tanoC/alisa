5e521a93120d9e22bc6df528edf204c3a910faba
techno-tanoC <develop.tanoc@gmail.com>
2020-05-18 21:31:19 +0900

    update progress

--- src/Progress.hs
+++ src/Progress.hs
@@ -8,7 +8,6 @@ import qualified Data.ByteString as BS
 import Data.Word (Word64)
 
 import Write
-import Reporter
 
 newtype Progress a = Progress (MVar (ProgressInner a))
 
@@ -16,23 +15,27 @@ data ProgressInner a = ProgressInner {
     name :: String,
     total :: Word64,
     size :: Word64,
-    dest :: a
+    buff :: a
 } deriving (Show, Eq)
 
 instance Writable a => Writable (Progress a) where
     write (Progress m) bs = modifyMVar_ m $ \inner -> do
         let added = size inner + (fromIntegral . BS.length $ bs)
-        write (dest inner) bs
+        print added
+        write (buff inner) bs
         return $ inner { size = added }
 
-new :: String -> a -> IO (Progress a)
-new name a = fmap Progress . newMVar $ ProgressInner name 0 0 a
+newProgress :: String -> a -> IO (Progress a)
+newProgress name a = fmap Progress . newMVar $ ProgressInner name 0 0 a
 
 setContentLength :: (Progress a) -> Word64 -> IO ()
 setContentLength (Progress pg) cl = modifyMVar_ pg update
     where update inner = return $ inner { total = cl }
 
-toReporter :: (Progress a) -> IO Reporter
-toReporter (Progress m) = fmap convert $ readMVar m
+toTuple :: (Progress a) -> IO (String, Word64, Word64)
+toTuple (Progress m) = fmap convert $ readMVar m
     where
-        convert (ProgressInner n t s _) = Reporter n t s
+        convert (ProgressInner n t s _) = (n, t, s)
+
+toInner :: (Progress a) -> IO (ProgressInner a)
+toInner (Progress m) = readMVar m
